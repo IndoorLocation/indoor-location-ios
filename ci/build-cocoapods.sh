7e1c6a44497b31ce23773945756132a5d6fd0099
Maxime Colmant <maxime.colmant@gmail.com>
2020-12-02 09:28:50 +0100

    chore: improving build with CI

--- ci/build-cocoapods.sh
+++ ci/build-cocoapods.sh
@@ -18,7 +18,7 @@ mkdir -p build indoorlocation/simulator indoorlocation/device
 echo -e "${BLUE}  > Building for Cocoapods...${NC}"
 
 # Build for simulator
-xcodebuild -configuration Release -sdk iphonesimulator CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= ONLY_ACTIVE_ARCH=NO ARCHS="x86_64" BITCODE_GENERATION_MODE="bitcode" DSTROOT="$(pwd)/build/dst" OBJROOT="$(pwd)/build/obj" OTHER_CFLAGS="-fembed-bitcode" SHARED_PRECOMPS_DIR="$(pwd)/build/shared" SYMROOT="$(pwd)/build/sym" build | xcpretty -s
+xcodebuild -configuration Release -sdk iphonesimulator CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64" BITCODE_GENERATION_MODE="bitcode" DSTROOT="$(pwd)/build/dst" OBJROOT="$(pwd)/build/obj" OTHER_CFLAGS="-fembed-bitcode" SHARED_PRECOMPS_DIR="$(pwd)/build/shared" SYMROOT="$(pwd)/build/sym" build | xcpretty -s
 if [ ! -d ./build/sym/Release-iphonesimulator/IndoorLocation.framework ]; then
   echo -e "${RED}  > The IndoorLocation framework for simulator does not exist${NC}"
   exit 1
@@ -30,7 +30,7 @@ rm -rf build
 mkdir build
 
 # Build for device
-xcodebuild -configuration Release -sdk iphoneos CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= ONLY_ACTIVE_ARCH=NO ARCHS="arm64" BITCODE_GENERATION_MODE="bitcode" DSTROOT="$(pwd)/build/dst" OBJROOT="$(pwd)/build/obj" OTHER_CFLAGS="-fembed-bitcode" SHARED_PRECOMPS_DIR="$(pwd)/build/shared" SYMROOT="$(pwd)/build/sym" build | xcpretty -s
+xcodebuild -configuration Release -sdk iphoneos CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= ONLY_ACTIVE_ARCH=NO ARCHS="arm64 armv7" BITCODE_GENERATION_MODE="bitcode" DSTROOT="$(pwd)/build/dst" OBJROOT="$(pwd)/build/obj" OTHER_CFLAGS="-fembed-bitcode" SHARED_PRECOMPS_DIR="$(pwd)/build/shared" SYMROOT="$(pwd)/build/sym" build | xcpretty -s
 if [ ! -d ./build/sym/Release-iphoneos/IndoorLocation.framework ]; then
   echo -e "${RED}  > The IndoorLocation framework for device does not exist${NC}"
   exit 1
